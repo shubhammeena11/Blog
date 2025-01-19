ef65016fd704ca3e70fb8b285d4b5842cba583e6
shubhammeena11 <shubhammeena982@gmail.com>
2025-01-19 16:36:42 +0530

    first commit

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -36,7 +36,7 @@ function AllPosts() {
         <Container>
             <div className='flex flex-wrap'>
             {posts.map((post)=>(
-                <div key={post.$id} className='p-2 w-1/4'>
+                <div key={post.$id} className='p-2 md:w-1/4 w-full'>
                 <PostCard {...post} />
                 </div>
             ))}
